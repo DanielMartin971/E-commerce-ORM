52aa996c5de88f46ac1a072a79b09157ec3d1306
Daniel <danielmartin971337@gmail.com>
2022-12-15 12:49:14 -0800

    made all the seeds for the initialization of the database

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-    id: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
